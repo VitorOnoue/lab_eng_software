311e37a76d7ca76bffb3f238f16ce9169c9c12d0
lucas.braga <lucas.braga@alloha.com>
2024-10-02 15:43:42 -0300

    Update 4

--- src/App.test.js
+++ src/App.test.js
@@ -2,13 +2,13 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders upload link', () => {
-  render(<App />); // Remova o BrowserRouter, já que o App já contém o Router
-  const linkElement = screen.getByText(/Upload de Contas/i); // Verifique algo que realmente existe no App
+  render(<App />); 
+  const linkElement = screen.getByText(/Upload de Contas/i); 
   expect(linkElement).toBeInTheDocument();
 });
 
 test('renders relatorios link', () => {
-  render(<App />); // Outro teste para verificar o link de Relatórios
+  render(<App />); 
   const linkElement = screen.getByText(/Relatórios/i);
   expect(linkElement).toBeInTheDocument();
 });
