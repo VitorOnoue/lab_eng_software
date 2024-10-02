5abe2d08c8354022f90418965d04c05ddec1b75a
lucas.braga <lucas.braga@alloha.com>
2024-10-02 14:21:26 -0300

    Update 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import Upload from '../../lab_eng_software/src/Upload';
-import Relatorios from '../../lab_eng_software/src/Relatorios';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Upload from './Upload';
+import Relatorios from './Relatorios';
 
-const App = () => {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li><Link to="/upload">Upload de Contas</Link></li>
-            <li><Link to="/relatorios">Relatórios</Link></li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/upload" element={<Upload />} />
-          <Route path="/relatorios" element={<Relatorios />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-};
+const App = () => (
+  <Router>
+    <div>
+      <nav>
+        <ul>
+          <li><Link to="/upload">Upload de Contas</Link></li>
+          <li><Link to="/relatorios">Relatórios</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/relatorios" element={<Relatorios />} />
+      </Routes>
+    </div>
+  </Router>
+);
 
 export default App;
