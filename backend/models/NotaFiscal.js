67ac513565b90b76a1be74977429972651ff8c16
godines51 <142548893+godines51@users.noreply.github.com>
2024-10-27 18:11:51 -0300

    Update NotaFiscal.js

--- backend/models/NotaFiscal.js
+++ backend/models/NotaFiscal.js
@@ -1,1 +1,23 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
 
+const NotaFiscal = sequelize.define('NotaFiscal', {
+  companhia: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  dataEmissao: {
+    type: DataTypes.DATEONLY,
+    allowNull: false,
+  },
+  consumo: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  valorTotal: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+});
+
+module.exports = NotaFiscal;
